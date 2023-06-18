8fb4f6f74298f81db179851b1a85ffdbb2f7cba0
rania <raniabm21@gmail.com>
2023-06-19 02:10:14 +0500

    css modification

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React from "react"
+import mernlogo from "../Images/mernLogo.png"
 
 const Home = () => {
     return (
         <div>
-            <p className="pt-5">Welcome Home</p>
-            <h1>WE ARE MERN DEVELOPER</h1>
+            <p className="pt-5 center">Welcome Home</p>
+            <h1 className="pt-4 center">WE ARE MERN DEVELOPER</h1>
+            <div className="center pt-5">
+                <img className="App-logo App-logo-spin" src={mernlogo} alt='mernlogo' />
+            </div>
         </div>
     )
 }
