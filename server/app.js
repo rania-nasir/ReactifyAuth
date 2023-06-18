b57df8bea9e99f7ff7ce6792990b899c124b322b
rania <raniabm21@gmail.com>
2023-06-18 16:34:46 +0500

    async await

--- server/app.js
+++ server/app.js
@@ -20,31 +20,6 @@ app.use(express.json());
 // link to Router to make our route easy
 app.use(require('./router/auth'));
 
-// Middleware
-const middleware = (req, res, next) => {
-    console.log('Hello My Middleware')
-    next()
-}
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World from the server...')
-// })
-
-app.get('/signin', (req, res) => {
-    res.send('Hello Log In World from the server...')
-})
-
-app.get('/signup', (req, res) => {
-    res.send('Hello Registeration World from the server...')
-})
-
-app.get('/about', middleware, (req, res) => {
-    res.send('Hello About World from the server...')
-})
-
-app.get('/contact', (req, res) => {
-    res.send('Hello Contact World from the server...')
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}...`)
--- server/router/auth.js
+++ server/router/auth.js
@@ -7,8 +7,40 @@ const User = require('../model/userSchema');
 router.get('/', (req, res) => {
     res.send('Hello World from the server router...')
 })
+///////// Using Promises
+// router.post('/register', (req, res) => {
 
-router.post('/register', (req, res) => {
+//     const { name, email, phone, work, password, cpassword } = req.body;
+
+//     if (!name || !email || !phone || !work || !password || !cpassword) {
+//         res.status(422).json({ error: "Please Fill All The Fields Properly!" });
+//     }
+
+//     User.findOne({ email: email })
+//         .then((userExist) => {
+//             if (userExist) { return res.status(422).json('Email Already Exist!') }
+
+//             const user = new User({ name, email, phone, work, password, cpassword })
+
+//             user.save().then(() => {
+//                 res.status(201).json({ message: 'User Registered Successfully!' });
+//             }).catch((err) => {
+//                 res.status(500).json({ error: 'Failed To Registered!' })
+//             })
+//         }).catch((err) => {
+//             console.log(err);
+//         })
+
+
+//     // console.log(name);
+//     // console.log(email);
+//     // console.log(req.body);
+//     // res.json({ message: req.body });
+//     // res.send('My Registeration Page');
+// })
+
+/////////// Using Async Await
+router.post('/register', async (req, res) => {
 
     const { name, email, phone, work, password, cpassword } = req.body;
 
@@ -16,27 +48,46 @@ router.post('/register', (req, res) => {
         res.status(422).json({ error: "Please Fill All The Fields Properly!" });
     }
 
-    User.findOne({ email: email })
-        .then((userExist) => {
-            if (userExist) { return res.status(422).json('Email Already Exist!') }
+    try {
+        const userExist = await User.findOne({ email: email });
+        if (userExist) { return res.status(422).json('Email Already Exist!') }
 
-            const user = new User({ name, email, phone, work, password, cpassword })
+        const user = new User({ name, email, phone, work, password, cpassword })
 
-            user.save().then(() => {
-                res.status(201).json({ message: 'User Registered Successfully!' });
-            }).catch((err) => {
-                res.status(500).json({ error: 'Failed To Registered!' })
-            })
-        }).catch((err) => {
-            console.log(err);
-        })
-
-
-    // console.log(name);
-    // console.log(email);
-    // console.log(req.body);
-    // res.json({ message: req.body });
-    // res.send('My Registeration Page');
+        const userRegister = await user.save();
+        if (userRegister) {
+            res.status(201).json({ message: 'User Registered Successfully!' });
+        } else {
+            res.status(500).json({ error: 'Failed To Registered!' })
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
 })
+
+
+router.get('/signin', (req, res) => {
+    res.send('Hello Log In World from the server...')
+})
+
+router.get('/signup', (req, res) => {
+    res.send('Hello Registeration World from the server...')
+})
+
+// Middleware
+const middleware = (req, res, next) => {
+    console.log('Hello My Middleware')
+    next()
+}
+
+router.get('/about', middleware, (req, res) => {
+    res.send('Hello About World from the server...')
+})
+
+router.get('/contact', (req, res) => {
+    res.send('Hello Contact World from the server...')
+})
+
 
 module.exports = router;
