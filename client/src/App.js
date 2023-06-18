046c8dd008e15b9ddd92e3afa9ee036f4f7e802d
rania <raniabm21@gmail.com>
2023-06-18 23:28:16 +0500

    created responsive navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () =>{
   return(
-   <div>
-    <h1>Hello World from Rania</h1>
-   </div>
+    <>
+    <Navbar />
+  </>
   )
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <p>Welcome</p>
+            <h1>WE ARE MERN DEVELOPER</h1>
+        </div>
+    )
+}
+
+export default Home
