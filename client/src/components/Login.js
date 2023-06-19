2ce9e8cf50edf83b7a44e32c520e98758790b235
rania <raniabm21@gmail.com>
2023-06-19 23:23:51 +0500

    UI of signup and login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,37 @@
 import React from "react";
+import loginpic from "../Images/loginpic.jpg"
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-        <h1>WE ARE IN A LOGIN PAGE</h1>
-        </div>
+        <>
+            <section className="login">
+                <div className="container mt-5 center">
+                    <div className="login-content">
+                        <div className="login-form">
+                            <h2 className="form-title">Log In</h2>
+                            <figure className="App right">
+                                <img src={loginpic} alt="Login-Pic"></img>
+                                <figcaption><NavLink to='/signup' className="signup-image-link">Create a account</NavLink></figcaption>
+                            </figure>
+                            <form className="login-form right m-3" id="login-form" action=''>
+                                <div className="form-group">
+                                    <label htmlFor="email"><i class="material-symbols-outlined">mail</i> </label>
+                                    <input placeholder="Your Email" type="text" name="email" id="email" autoComplete="off"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password"><i class="material-symbols-outlined">lock</i> </label>
+                                    <input placeholder="Your Password" type="text" name="password" id="password" autoComplete="off"></input>
+                                </div>
+                                <div className="form-button submit-div">
+                                    <input type="submit" name="login" id="login" className="btn btn-primary" value='Login'></input>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,54 @@
 import React from "react";
+import signpic from "../Images/registerationpic.svg"
+import { NavLink } from "react-router-dom";
 
 const Signup = () => {
     return (
-        <div>
-        <h1>WE ARE IN A REGISTERATION PAGE</h1>
-        </div>
+        <>
+            <section className="signup">
+                <div className="container mt-5 center">
+                    <div className="signup-content">
+                        <div className="signup-form">
+                            <h2 className="form-title">Sign up</h2>
+                            <form className="register-form right m-3" id="register-form" action=''>
+                                <div className="form-group">
+                                    <label htmlFor="name"><i class="material-symbols-outlined">person_2</i> </label>
+                                    <input placeholder="Your Name" type="text" name="name" id="name" autoComplete="off"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="email"><i class="material-symbols-outlined">mail</i> </label>
+                                    <input placeholder="Your Email" type="text" name="email" id="email" autoComplete="off"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="phone"><i class="material-symbols-outlined">call</i> </label>
+                                    <input placeholder="Your Phone" type="text" name="phone" id="phone" autoComplete="off"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="work"><i class="material-symbols-outlined">work</i> </label>
+                                    <input placeholder="Your Profession" type="text" name="work" id="work" autoComplete="off"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password"><i class="material-symbols-outlined">lock</i> </label>
+                                    <input placeholder="Your Password" type="text" name="password" id="password" autoComplete="off"></input>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="cpassword"><i class="material-symbols-outlined">lock</i> </label>
+                                    <input placeholder="Confirm Your Password" type="text" name="cpassword" id="cpassword" autoComplete="off"></input>
+                                </div>
+                                <div className="form-button submit-div">
+                                    <input type="submit" name="signup" id="signup" className="btn btn-primary" value='Register'></input>
+                                </div>
+                            </form>
+
+                            <figure className="App right">
+                                <img src={signpic} alt="Registeration-Pic"></img>
+                                <figcaption><NavLink to='/Login.js' className="signup-image-link">I am already register</NavLink></figcaption>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
