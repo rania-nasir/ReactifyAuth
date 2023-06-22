d3c177f57c34d742167e1a5f3b1d54b46256bdfa
rania <raniabm21@gmail.com>
2023-06-22 17:08:25 +0500

    Store Registration Data in Frontend React State

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import signpic from "../Images/registerationpic.svg"
 import { NavLink } from "react-router-dom";
 
 const Signup = () => {
+
+    const [user, setUser] = React.useState({
+        name: "", email: "", phone: "", work: "", password: "", cpassword: ""
+    });
+
+    let name, value;
+    const handleInputs = (e) => {
+        console.log(e);
+        name = e.target.name;
+        value = e.target.value;
+        setUser({ ...user, [name]: value });
+        // console.log(setUser({ ...user, [name]: value }));
+    }
+
     return (
         <>
             <section className="signup">
@@ -12,28 +26,40 @@ const Signup = () => {
                             <h2 className="form-title">Sign up</h2>
                             <form className="register-form right m-2" id="register-form" action=''>
                                 <div className="form-group">
-                                    <label htmlFor="name"><i class="material-symbols-outlined">person_2</i> </label>
-                                    <input placeholder="Your Name" type="text" name="name" id="name" autoComplete="off"></input>
+                                    <label htmlFor="name"><i className="material-symbols-outlined">person_2</i> </label>
+                                    <input placeholder="Your Name" type="text" name="name" id="name" autoComplete="off"
+                                        value={user.name}
+                                        onChange={handleInputs} />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="email"><i class="material-symbols-outlined">mail</i> </label>
-                                    <input placeholder="Your Email" type="text" name="email" id="email" autoComplete="off"></input>
+                                    <label htmlFor="email"><i className="material-symbols-outlined">mail</i> </label>
+                                    <input placeholder="Your Email" type="text" name="email" id="email" autoComplete="off"
+                                        value={user.email}
+                                        onChange={handleInputs} />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="phone"><i class="material-symbols-outlined">call</i> </label>
-                                    <input placeholder="Your Phone" type="text" name="phone" id="phone" autoComplete="off"></input>
+                                    <label htmlFor="phone"><i className="material-symbols-outlined">call</i> </label>
+                                    <input placeholder="Your Phone" type="text" name="phone" id="phone" autoComplete="off"
+                                        value={user.phone}
+                                        onChange={handleInputs} />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="work"><i class="material-symbols-outlined">work</i> </label>
-                                    <input placeholder="Your Profession" type="text" name="work" id="work" autoComplete="off"></input>
+                                    <label htmlFor="work"><i className="material-symbols-outlined">work</i> </label>
+                                    <input placeholder="Your Profession" type="text" name="work" id="work" autoComplete="off"
+                                        value={user.work}
+                                        onChange={handleInputs} />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="password"><i class="material-symbols-outlined">lock</i> </label>
-                                    <input placeholder="Your Password" type="text" name="password" id="password" autoComplete="off"></input>
+                                    <label htmlFor="password"><i className="material-symbols-outlined">lock</i> </label>
+                                    <input placeholder="Your Password" type="text" name="password" id="password" autoComplete="off"
+                                        value={user.password}
+                                        onChange={handleInputs} />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="cpassword"><i class="material-symbols-outlined">lock</i> </label>
-                                    <input placeholder="Confirm Your Password" type="text" name="cpassword" id="cpassword" autoComplete="off"></input>
+                                    <label htmlFor="cpassword"><i className="material-symbols-outlined">lock</i> </label>
+                                    <input placeholder="Confirm Your Password" type="text" name="cpassword" id="cpassword" autoComplete="off"
+                                        value={user.cpassword}
+                                        onChange={handleInputs} />
                                 </div>
                                 <div className="form-button submit-div">
                                     <input type="submit" name="signup" id="signup" className="btn btn-primary" value='Register'></input>
@@ -51,5 +77,6 @@ const Signup = () => {
         </>
     )
 }
+
 
 export default Signup
