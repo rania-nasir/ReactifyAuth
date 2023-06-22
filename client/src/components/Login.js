1856f1a1d65a8f6c32abacea05d295838091ffaf
rania <raniabm21@gmail.com>
2023-06-22 22:37:27 +0500

    Login System with JWT & Stored Cookie

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import loginpic from "../Images/loginpic.jpg"
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loginUser = async (e) => {
+        e.preventDefault(); //to stop the reload
+        const res = await fetch('/signin', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email, password
+            })
+        })
+
+        await res.json();
+        if (res.status === 400 || res.status === 422) {
+            window.alert('Invalid Credentials');
+            console.log('Invalid Credentials');
+        } else if (res.status === 200) {
+            window.alert('Login Successful');
+            console.log('Login Successful');
+            navigate("/");
+        } else {
+            window.alert('Something went wrong');
+            console.log('Something went wrong');
+        }
+    }
     return (
         <>
             <section className="login">
@@ -14,17 +43,20 @@ const Login = () => {
                                 <img src={loginpic} alt="Login-Pic"></img>
                                 <figcaption><NavLink to='/signup' className="signup-image-link">Create an account</NavLink></figcaption>
                             </figure>
-                            <form className="login-form right m-3" id="login-form" action=''>
+                            <form method="POST" className="login-form right m-3" id="login-form">
                                 <div className="form-group">
                                     <label htmlFor="email"><i className="material-symbols-outlined">mail</i> </label>
-                                    <input placeholder="Your Email" type="text" name="email" id="email" autoComplete="off"></input>
+                                    <input value={email} onChange={(e) => setEmail(e.target.value)}
+                                        placeholder="Your Email" type="text" name="email" id="email" autoComplete="off"></input>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="password"><i className="material-symbols-outlined">lock</i> </label>
-                                    <input placeholder="Your Password" type="text" name="password" id="password" autoComplete="off"></input>
+                                    <input value={password} onChange={(e) => setPassword(e.target.value)}
+                                        placeholder="Your Password" type="text" name="password" id="password" autoComplete="off"></input>
                                 </div>
                                 <div className="form-button submit-div">
-                                    <input type="submit" name="login" id="login" className="btn btn-primary" value='Login'></input>
+                                    <input type="submit" name="login" id="login" className="btn btn-primary" value='Login'
+                                        onClick={loginUser}></input>
                                 </div>
                             </form>
                         </div>
